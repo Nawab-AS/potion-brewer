f1351a0a2d0cac3575e30a427a074a6c968106a5
Hamid Syed <59747708+Nawab-AS@users.noreply.github.com>
2025-10-12 18:47:16 +0000

    stores ingredients in the 'cauldron'

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const { createApp, ref } = Vue;
 
+activeIngredient = ref(null);
 const ingredients = ref([
     'Water',
     'Salt',
 ]);
+const storedIngredients = ref([]);
 
-activeIngredient = ref(null);
-
-
+// initialize ingredients
 for (let i = 1; i <= 50; i++) {
     ingredients.value.push(`Ingredient ${i}`);
 }
+
+
+
+// get cauldron coordinates
+let cauldronPos = {};
+const getCauldronPos = () => {
+    cauldronPos = document.getElementById('cauldron').getBoundingClientRect();
+};
+window.addEventListener('resize', getCauldronPos);
+getCauldronPos();
+
 
 
 // Mouse position tracking
@@ -19,9 +30,24 @@ document.addEventListener('mousemove', (event) => {
     mousePos.value = {x: event.clientX, y: event.clientY};
 });
 
-document.addEventListener('mousedown', (e) => {e.preventDefault();});
+document.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+});
 document.addEventListener('mouseup', (e) => {
     e.preventDefault();
+    if (!activeIngredient.value) return; // continue if an ingredient is being carried
+
+    if (mousePos.value.x - 100 >= cauldronPos.left && mousePos.value.x + 100 <= cauldronPos.right &&
+            mousePos.value.y - 25 >= cauldronPos.top && mousePos.value.y + 25 <= cauldronPos.bottom) {
+        // mouse in in cauldron
+        storedIngredients.value.push({
+            name: activeIngredient.value,
+            x: mousePos.value.x-96,
+            y: mousePos.value.y-21,
+            id: Date.now(),
+        });
+
+    }
     activeIngredient.value = null;
 });
 
@@ -38,6 +64,7 @@ createApp({
         return {
             ingredients,
             activeIngredient,
+            storedIngredients,
             ingredientClicked,
             mousePos,
         };
